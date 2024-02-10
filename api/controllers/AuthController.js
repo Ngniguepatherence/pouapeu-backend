255de17f188bfe0096ff18eb2823d77877e25242
Ngniguepa Therence <ngniguepafaha@gmail.com>
2024-02-10 18:06:49 +0100

    deploiements

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -5,6 +5,24 @@ const jwt = require('jsonwebtoken');
 const secret = "je vous en prie";
 
 const authControl = {
+    Auth: async (req,res) => {
+        const {email,password} = req.body;
+        try {
+
+            const user = await profil.findOne({email});
+            if(!user) {
+                return res.status(401).json({error: 'Invalid email'});
+            }
+            if(user.password == password) {
+            const token = jwt.sign({userId: user}, secret, {expiresIn: '1h'});
+            console.log(token);
+            res.status(200).json({token, expiresIn: 3600});
+            }
+        }
+        catch(error) {
+            res.status(500).json({error: 'Internal ERROR'});
+        }
+    },
     Login: async (req,res) => {
         const { email, expires } = req.body;
         const user = await profil.findOne({email});
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController  = require('../controllers/AuthController');
 
 router.post('/google',authController.Login);
+router.post('/login',authController.Auth);
 router.post('/logout',authController.Logout);
 
 
